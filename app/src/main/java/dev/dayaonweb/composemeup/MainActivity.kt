fb0c22b2491d5542d366596467f45e6efc295edf
Damercy <adhikaridayamoy@gmail.com>
2021-12-09 05:31:14 +0530

    Update deps & basic layout

--- app/src/main/java/dev/dayaonweb/composemeup/MainActivity.kt
+++ app/src/main/java/dev/dayaonweb/composemeup/MainActivity.kt
@@ -3,11 +3,18 @@ package dev.dayaonweb.composemeup
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.dayaonweb.composemeup.ui.theme.ComposeMeUpTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,16 +31,37 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp() {
+    var shouldShowHomeScreen by rememberSaveable { mutableStateOf(false) }
+    if (shouldShowHomeScreen)
+        HomeScreen()
+    else
+        LoginScreen {
+            shouldShowHomeScreen = false
+        }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ComposeMeUpTheme {
-        Greeting("Android")
+fun LoginScreen(
+    onContinueBtnClick: () -> Unit = {}
+) {
+    Column{
+        Text(
+            modifier = Modifier
+                .fillMaxSize(),
+            text = "Login Screen",
+        )
     }
+}
+
+@Composable
+fun HomeScreen() {
+
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
 }
