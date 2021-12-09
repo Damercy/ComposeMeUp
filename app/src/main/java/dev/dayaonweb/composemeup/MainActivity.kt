d7d88bc53776eb87f3573fd90b21af6a7f591976
Damercy <adhikaridayamoy@gmail.com>
2021-12-10 02:27:45 +0530

    Add basic modifiers & screens

--- app/src/main/java/dev/dayaonweb/composemeup/MainActivity.kt
+++ app/src/main/java/dev/dayaonweb/composemeup/MainActivity.kt
@@ -3,7 +3,9 @@ package dev.dayaonweb.composemeup
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -12,9 +14,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import dev.dayaonweb.composemeup.ui.theme.ComposeMeUpTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +29,7 @@ class MainActivity : ComponentActivity() {
             ComposeMeUpTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    MyApp()
                 }
             }
         }
@@ -42,26 +47,51 @@ fun MyApp() {
         }
 }
 
-@Preview(showBackground = true)
+
 @Composable
 fun LoginScreen(
     onContinueBtnClick: () -> Unit = {}
 ) {
-    Column{
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceAround,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(
-            modifier = Modifier
-                .fillMaxSize(),
             text = "Login Screen",
+            style = MaterialTheme.typography.h2,
+            fontWeight = FontWeight.SemiBold,
+            textAlign = TextAlign.Center
+        )
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.3f)
+                .background(Color.Cyan)
+        )
+        Button(onClick = onContinueBtnClick) {
+            Text(
+                text = "To Home Screen",
+                style = MaterialTheme.typography.button
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Home screen",
+            style = MaterialTheme.typography.h2,
+            textAlign = TextAlign.Center
         )
     }
 }
 
-@Composable
-fun HomeScreen() {
-
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
